c5a3b4190bb617d3ebdbafdfc34905f339f3d441
Yeboah Martin <ymartin951@gmail.com>
2022-07-05 16:29:49 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - calls the _putchar function
+ * _putchar - print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar("_putchar\n");
+
+	return 0;
+}
