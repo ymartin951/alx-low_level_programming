578c1f0718f441ebcbdf1f8e58d88a6a6004f469
Yeboah Martin <ymartin951@gmail.com>
2022-07-05 17:47:22 -0700

    _putchar file modified

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,12 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * main - calls the _putchar function
- * _putchar - print _putchar
+ * main - Prints _putchar as a message.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	_putchar("_putchar\n");
-
 	return (0);
 }
