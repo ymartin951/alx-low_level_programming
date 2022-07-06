131eacb8510fac56dcec73bfdcc6da967d6559ef
Yeboah Martin <ymartin951@gmail.com>
2022-07-05 20:26:03 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet ten times.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
