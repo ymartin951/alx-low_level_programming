866d6a89314191c0bba80974139b48c89812da62
Yeboah Martin <ymartin951@gmail.com>
2022-07-07 08:24:23 -0700

    0-main.c file modified

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
-
 /**
- * main -  check if number passed to positive_or_nagative() function.
- *
- * Return: Always 0 (Success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
-	positive_or_negative(9);
-}
+	int i;
 
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
