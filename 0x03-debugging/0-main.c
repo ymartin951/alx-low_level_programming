22e77452a7df063fe22699116bb2fc22b618a90d
Yeboah Martin <ymartin951@gmail.com>
2022-07-07 08:14:47 -0700

    0-main.c and main.h files added

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main -  check if number passed to positive_or_nagative() function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	positive_or_negative(9);
+}
+
